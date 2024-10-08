c74458ba53e4d6b4e3a90df619445a968f07a40b
chaitanya <chaitanyadesireddy1@gmail.com>
2024-10-08 18:16:56 +0530

    third commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 
 }
